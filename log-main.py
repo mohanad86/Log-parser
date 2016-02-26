8213b1df2f2725d9f378142fd05eda381348ad80
Mohanad86 <capmohanad@hotmail.com>
2016-02-26 23:23:20 +0200

    Define the humanize function

--- log-main.py
+++ log-main.py
@@ -42,10 +42,23 @@ for filename in os.listdir(root):
                         d[keyword] = d[keyword] + 1
                     except KeyError:
                         d[keyword] = 1
-                    break # Stop searching for other keywords
+                    break 
         except ValueError:
-            pass # This will do nothing, needed due to syntax
-
+            pass 
+def humanize(bytes):
+    if bytes < 1024:
+        return "%d B" % bytes
+    elif bytes < 1024 ** 2:
+        return "%.1f KB" % (bytes / 1024.0)
+    elif bytes < 1024 ** 3:
+        return "%.1f MB" % (bytes / 1024.0 ** 2)
+    else:
+        return "%.1f GB" % (bytes / 1024.0 ** 3)
+for filename in os.listdir("."):
+    mode, inode, device, nlink, vid, gid, size, atime, mtime, ctime = os.stat(filename) 
+    
+print filename, humanize(size)
+print "***********************"
 print "Top 5 USERS", total
 print "***********************"
 print "Total lines:", total
