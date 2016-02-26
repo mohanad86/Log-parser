5c1303e0f8fd8b4348a331a28a5670d2da950018
Mohanad Aly <capmohanad@hotmail.com>
2016-02-26 22:11:53 +0200

    Delete log.py

--- log.py
+++
@@ -1,4 +0,0 @@
-import gzip
-# gzip.open will give you a file object which transparently uncompresses the file as it's read
-for line in gzip.open("/var/log/apache2/access.log.1.gz"):
-    print line
