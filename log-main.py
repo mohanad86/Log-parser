a090d0d205259cb1e560f3e3fb5956038b1d93be
Mohanad86 <capmohanad@hotmail.com>
2016-02-26 23:08:32 +0200

    Parser

--- log-main.py
+++ log-main.py
@@ -1,6 +1,14 @@
 import os
 import urllib
 import gzip
+import argparse
+import gzip
+parser = argparse.ArgumentParser(description='Apache2 log parser.')
+parser.add_argument('--path', help='Path to Apache2 log files', default="/home/malyhass/Documents")
+parser.add_argument('--top-urls', help="Find top URL-s", action='store_true')
+parser.add_argument('--geoip', help ="Resolve IP-s to country codes", action='store_true')
+parser.add_argument('--verbosity', help="Increase verbosity", action="store_true")
+args = parser.parse_args()
 #this is the directory where is the log files locate,
 root = "/home/mohanad"
 
@@ -8,7 +16,6 @@ keywords = "Windows", "Linux", "OS X", "Ubuntu", "Googlebot", "bingbot", "Androi
 d = {} 
 urls = {}
 total = 0
-import gzip
 for filename in os.listdir(root):
     if not filename.startswith("access.log"):
         print "Skipping unknown file:", filename
@@ -40,9 +47,9 @@ for filename in os.listdir(root):
             pass # This will do nothing, needed due to syntax
 
 print "Top 5 USERS", total
-print "-----------------------"
+print "***********************"
 print "Total lines:", total
-print "-----------------------"
+print "***********************"
 print "TOP 5 PAGES", total
 result = urls.items()
 result.sort(key = lambda item:item[1], reverse=True)
